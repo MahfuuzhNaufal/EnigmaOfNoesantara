0052f93c9cb32eaccd655090bd5a1bbe1907405a
notRocky <mahfuuzhnaufal@gmail.com>
2023-07-13 21:46:16 +0700

    Musuh

--- Assets/Script/AIMusuh.cs
+++ Assets/Script/AIMusuh.cs
@@ -7,6 +7,8 @@ public class AIMusuh : MonoBehaviour
     public GameObject player;
     public float speed;
     private float distance;
+    public int maxHealth = 100;
+    int currentHealth; 
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +16,22 @@ public class AIMusuh : MonoBehaviour
         Vector2 direction = player.transform.position - transform.position;
 
         transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
+
+        currentHealth = maxHealth;
+    }
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Debug.Log("musuh mati");
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Script/PlayerAttack.cs
+++ Assets/Script/PlayerAttack.cs
@@ -5,19 +5,44 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
     public Animator animator;
+    public Transform attackPoint;
+    
+    public LayerMask enemyLayers;
+
+    public float attackRange = 0.5f;
+    public int attackDamage = 40;
+
+    public float attackRate = 2f;
+    float nextAttackTime = 0f; 
 
     void Update()
     {
-        // Cek input serangan (misalnya tombol Attack)
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if(Time.time >= nextAttackTime)
         {
-            // Aktifkan trigger AttackTrigger untuk memulai animasi serangan
-            Attack();
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                Attack();
+                nextAttackTime = Time.time + 1f / attackRate;
+            }
         }
+
     }
 
     void Attack()
     {
         animator.SetTrigger("AttDepan");
+        Collider2D[] hitEnemies =  Physics2D.OverlapCircleAll(attackPoint.position, attackRange,enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<AIMusuh>().TakeDamage(attackDamage);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
     }
 }
